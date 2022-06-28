f893ed5ab6ed388b3eba91260a42df2c67911a33
kwan2 <jgh9987@naver.com>
2022-06-29 01:19:22 +0900

    [FEAT]Setting gradle

---
+++ cc.gg/src/test/java/cced/cc/gg/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cced.cc.gg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
